84af967837c6c2c129515015b7d90abc55dda2fe
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-08-30 11:33:46 -0300

    Agregué rutas de prueba

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), ()=>{
     console.log(`Estamos en el puerto ${app.get('port')}`)
 })
+
+//middlewares
+
+//rutas
+app.get('/', (req, res)=>{
+    res.send('primera peticion get')
+})
+app.get('/prueba', (req, res)=>{
+    res.send('otra peticion get')
+})
