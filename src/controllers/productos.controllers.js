73e756d802499f21c58438422dca7e1a68011756
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-09-01 11:37:46 -0300

    Agregue el archivo de ruta para la peticion post

---
+++ src/controllers/productos.controllers.js
@@ -0,0 +1,11 @@
+export const crearProducto = (req, res)=>{
+    res.send('crear un producto para la base de datos')
+}
+
+export const listarProductos = (req, res)=>{
+    res.send('enviar lista de productos')
+}
+
+export const obtenerProductos = (req, res)=>{
+    res.send('aqui envio un objeto producto')
+}
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,14 +1,20 @@
 import { Router } from "express";
+import { crearProducto, listarProductos, obtenerProductos } from "../controllers/productos.controllers";
 
 const router = Router();
 
 //crear todas las rutas de los productos
-//dominio+ /apicafe + /
+//dominio+ /apicafe + /productos
 router
-.route('/')
-.get((req, res)=>{
-     res.send('primera peticion get')
-})
+.route('/productos')
+.get(listarProductos)
+.post(crearProducto);
+
+router
+.route('/productos/:id')
+.get(obtenerProductos)
+.put()
+.delete();
 
 export default router;
 
