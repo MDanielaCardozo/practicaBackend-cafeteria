50cb83750ff5536dcd6ee7e919c4af47829fe4e8
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-09-08 11:17:49 -0300

    Agregué las validaciones con express validator

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,9 +1,19 @@
 import Producto from "../models/producto";
+import { validationResult } from 'express-validator';
 
 export const crearProducto = async(req, res)=>{
     try {
         console.log(req.body)
         //validacion 
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json(
+                {
+                    errors: errors.array() //devuelve la lista de errores
+                    //errors: errors.mapped() //devuelve el primer error
+                }
+         )
+        }
         //crear un objeto para guardar en la base de datos
         const productoNuevo = new Producto({
             nombreProducto:req.body.nombreProducto,
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,28 +1,55 @@
 import { Router } from "express";
-import { borrarProducto, crearProducto, editarProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers";
+import {
+  borrarProducto,
+  crearProducto,
+  editarProducto,
+  listarProductos,
+  obtenerProducto,
+} from "../controllers/productos.controllers";
+import { check } from "express-validator";
 
 const router = Router();
 
 //crear todas las rutas de los productos
 //dominio+ /apicafe + /productos
 router
-.route('/productos')
-.get(listarProductos)
-.post(crearProducto);
+  .route("/productos")
+  .get(listarProductos)
+  .post(
+    [
+      check(
+        "nombreProducto",
+        "El nombre del producto es obligatorio"
+      ).notEmpty(),
+      check(
+        "nombreProducto",
+        "El producto debe tener entre 2 y 50 caracteres"
+      ).isLength({ min: 2, max: 50 }),
+      check("precio", "El precio es un valor obligatorio").notEmpty(),
+      check("precio").custom((value) => {
+        if (value >= 0 && value <= 9000) {
+          return true;
+        } else {
+          throw new Error("El precio debe estar entre 0 y 9000");
+        }
+      }),
+    ],
+    crearProducto
+  );
 
 router
-.route('/productos/:id')
-.get(obtenerProducto)
-.put(editarProducto)
-.delete(borrarProducto);
+  .route("/productos/:id")
+  .get(obtenerProducto)
+  .put(editarProducto)
+  .delete(borrarProducto);
 
 export default router;
 
 // app.get('/', (req, res)=>{
-    // res.send('primera peticion get')
+// res.send('primera peticion get')
 //})
 //app.get('/prueba', (req, res)=>{
-  //  res.send('otra peticion get')
+//  res.send('otra peticion get')
 //})
 
 //app.post('/')
